49c7a527fe21a01b561363de8a657de86daff545
Tocivlasok <simona.korkobcova@gmail.com>
2020-05-23 09:50:19 +0100

    code adapted according to docstring linting standards

--- playWithString.py
+++ playWithString.py
@@ -1,22 +1,33 @@
+"""Spyder Editor."""
+
 import argparse
 
+
 def reverseStr(strName):
-	strToTransform = (''.join(reversed(strName)))
-	return strToTransform
+    """Return reversed string."""
+    strToTransform = (''.join(reversed(strName)))
+    return strToTransform
+
 
 def upperCaseStr(strName):
-	strToTransform = strName.upper()
-	return strToTransform
+    """Return upperCased string."""
+    strToTransform = strName.upper()
+    return strToTransform
+
 
 def Main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("strToTransform", help = "Enter a string to be transformed: ", type = str)
-	args = parser.parse_args()
+    """Prompts the user to enter his/her string.
 
-	print("Your string Reversed: " + reverseStr(args.strToTransform))
-	print("Your string UpperCased: " + upperCaseStr(args.strToTransform))
-	print("Your string Transofrmed in peculiar way: " + upperCaseStr(reverseStr(args.strToTransform)))
+    Converts it in 3 different ways.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("strToTransform", help="Enter a string to be transformed: ", type=str)
+    args = parser.parse_args()
+
+    print("Your string Reversed: " + reverseStr(args.strToTransform))
+    print("Your string UpperCased: " + upperCaseStr(args.strToTransform))
+    print("Your string Transofrmed in peculiar way: " + upperCaseStr(reverseStr(args.strToTransform)))
+
 
 if __name__ == '__main__':
-	Main()
-	
+    Main()
